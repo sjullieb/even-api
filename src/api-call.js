38fa4a2f0d317eb27996ea51a7bc34d1d87dc3b0
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-04-13 21:52:19 -0700

    create loans array from json file

--- src/api-call.js
+++ src/api-call.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
+import { Loan, Originator, Image } from './loan.js';
 
 var apiInterval;
+var loans = [];
 
-export function apiCallPost(data, postError, getError){
+export function apiCallPost(data, postError, getError, displayLoans){
   let settings = {
     "async": true,
     "crossDomain": true,
@@ -18,7 +20,7 @@ export function apiCallPost(data, postError, getError){
   }
 
   $.ajax(settings).done(function (response) {
-    apiInterval = setInterval(apiCallGet, 1000, response.uuid, getError);
+    apiInterval = setInterval(apiCallGet, 1000, response.uuid, getError, displayLoans);
   }).fail(function(error){
     let text = error.responseText;
     let quotes = 0;
@@ -35,7 +37,7 @@ export function apiCallPost(data, postError, getError){
   });
 }
 
-export function apiCallGet(uuid, getError){
+export function apiCallGet(uuid, getError, displayLoans){
   let settings = {
     "async": true,
     "crossDomain": true,
@@ -48,6 +50,8 @@ export function apiCallGet(uuid, getError){
   }
 
   $.ajax(settings).done(function (response) {
+    fillLoansArray(response);
+    displayLoans(loans);
     if(response.pendingResponses.length === 0){
       clearInterval(apiInterval);
     }
@@ -66,4 +70,54 @@ export function apiCallGet(uuid, getError){
     }
     getError(strMessage);
   });
+}
+
+function fillLoansArray(response){
+  loans = [];
+  console.log(response);
+  if (response.loanOffers.length > 0){
+    for(let i = 0; i < response.loanOffers.length; i++){
+      let loan = response.loanOffers[i];
+      let newOriginator = new Originator(loan.originator.name);
+
+      if (loan.originator.hasOwnProperty('description')){
+        newOriginator.description = loan.originator.description;
+      }
+
+      if (loan.originator.hasOwnProperty('disclaimer')){
+        newOriginator.disclaimer = loan.disclaimer;
+      }
+
+      if(loan.originator.hasOwnProperty('images')){
+        for(let j = 0; j < loan.originator.images.length; j++){
+            let newImage = new Image(loan.originator.images[j].sizeKey, loan.originator.images[j].url);
+            newOriginator.images.push(newImage);
+        }
+      }
+
+      let newLoan = new Loan(newOriginator, loan.preQualified, loan.preApproved, loan.termLength, loan.termUnit);
+
+      if(loan.hasOwnProperty('maxAmount')){
+          newLoan.maxAmount = loan.maxAmount;
+      }
+      if(loan.hasOwnProperty('minAmount')){
+          newLoan.minAmount = loan.minAmount;
+      }
+      if(loan.hasOwnProperty('maxApr')){
+          newLoan.maxApr = loan.maxApr;
+      }
+      if(loan.hasOwnProperty('minApr')){
+          newLoan.minApr = loan.minApr;
+      }
+      if(loan.hasOwnProperty('meanApr')){
+          newLoan.meanApr = loan.meanApr;
+      }
+      if(loan.hasOwnProperty('url')){
+          newLoan.url = loan.url;
+      }
+
+      loans.push(newLoan);
+    }
+  }
+  //console.log(loans);
 }
